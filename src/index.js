91ad424ae2c42b681333bc3e9b7c32813cce4666
everton-lacerda <everton_ml@outlook.com>
2021-03-02 16:24:24 -0300

    cors fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ const routes = require('./routes')
 const port = process.env.PORT || 3000
 
 const app = express()
-app.use(cors()) 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    app.use(cors()) 
+    next();
+})
 
 const server = require('http').Server(app)
 // const io = require('socket.io')(server)
