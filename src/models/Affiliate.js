60e910b0dce17b48a8eae571bf273e076e9f36a0
everton-lacerda <everton_ml@outlook.com>
2021-03-02 16:07:48 -0300

    fix(model): fix  in the model

--- src/models/Affiliate.js
+++ src/models/Affiliate.js
@@ -1,32 +1,50 @@
 const {Schema, model } = require('mongoose')
 
 const Affiliatechema = new Schema({
+
+
+
+    
     affiliateName: { type: String, required: true,},
     cpf: { type: String, required: true }, 
+    cnpj: { type: String, required: true }, 
     rg: { type: String, required: true }, 
     email: { type: String, required: true }, 
+    cep: { type: String, required: true }, 
     yearEntered: { type: Number, required: true }, 
     birthdate: { type: Date, required: true }, 
+    registerDate: { type: Date, required: true }, 
     address: { type: String, required: true }, 
-    addressComplement: { type: String, required: true }, 
+    addressComplement: { type: String }, 
+    addressHouseBorn: { type: String, required: true }, 
+    addressTemple: { type: String, required: true }, 
     district: { type: String, required: true }, 
+    muchTime: { type: String, required: true }, 
     city: { type: String, required: true }, 
     phoneNumber: { type: String, required: true }, 
+    whatsappNumber: { type: String, required: true }, 
+    openHouse: { type: String, required: true }, 
     whatsappNumber: { type: String }, 
     dadOrMom: { type: String, required: true }, 
+    dadOrMomPhone: { type: String, required: true }, 
     axeTempleName: { type: String, required: true }, 
     observation: { type: String, required: true }, 
     contactName: { type: String }, 
+    state: { type: String }, 
+    voterRegistration: { type: String }, 
     contactNumber: { type: String }, 
+    whichNationId: { type: String }, 
 
-    maritalStatusId: { type: Number, required: true }, 
+    maritalStatusId: { type: Number}, 
+    religiosityId: { type: Number}, 
+    regularized: { type: Number}, 
     maritalStatus: { type: String, required: true },
     
-    affiliateTypeId: { type: Number, required: true }, 
-    affiliateType: { type: String, required: true },
+    affiliateTypeId: { type: Number}, 
+    affiliateType: { type: String },
     
     priestlyHierarchyId: { type: Number, required: true }, 
-    priestlyHierarchy: { type: String, required: true },
+    priestlyHierarchy: { type: String},
 
     userId: { type: String, required: true },
     
