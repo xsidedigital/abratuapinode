5543cad168434a1da6dce649a3ec58242a1a50b5
everton-lacerda <everton_ml@outlook.com>
2021-03-03 15:09:04 -0300

    fix cors change res for req

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://www.abratu.app.br')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    req.header('Access-Control-Allow-Origin', 'http://www.abratu.app.br')
+    req.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    req.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next();
 })
 
